f1716bc17b0c629f73957199b932a1508a268db8
Prashant Patil <prashpatil702@gmail.com>
2026-01-26 17:49:10 +0530

    Added custom linked list in java

---
+++ src/basicDsa/LinkedList.java
@@ -0,0 +1,67 @@
+package basicDsa;
+
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int data)
+	{
+		this.data = data;
+		this.next = null;
+	}
+	
+}
+
+
+public class LinkedList {
+	
+	static Node head = null;
+	
+	public static void addNode(int data)
+	{
+		if(head == null)
+		{
+			head = new Node(data);
+		}
+		else
+		{
+			Node temp = head;
+			while(temp.next!=null)
+			{
+				temp = temp.next;
+			}
+			temp.next = new Node(data);
+		}
+	}
+	
+	public static void printList(LinkedList list)
+	{
+		Node temp = list.head;
+		while(temp!=null)
+		{
+			System.out.println(temp.data);
+			temp = temp.next;
+
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		
+		list.addNode(1);
+		list.addNode(2);
+		list.addNode(3);
+		list.addNode(4);
+		list.addNode(5);
+		
+		list.printList(list);
+		
+		
+		
+		
+	}
+
+}
